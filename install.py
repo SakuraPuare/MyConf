91b00d8407dda5f71aed5415c2d2b95bb2e38369
Steven Moder <java20131114@gmail.com>
2024-05-22 10:42:26 +0800

    feat: add script for installing dotfiles and configuration files
    This commit adds a new script, `install.py`, which is responsible for installing dotfiles and configuration files. The script iterates through the files in the current directory, excluding blacklisted files, and creates symbolic links to the corresponding files in the user's home directory. This allows for easy setup of the development environment and configuration of various tools and utilities.
    
    Co-authored-by: [Author Name]

---
+++ install.py
@@ -0,0 +1,34 @@
+import pathlib
+import os
+
+black_list = ['.git','LICENSE','README.md','install.sh','install.py']
+
+install_dest = pathlib.Path('~').expanduser()
+
+def mk_symlink(path):
+    src: pathlib.Path = path.absolute()
+    dest: pathlib.Path = install_dest / path
+    if dest.exists(follow_symlinks=False):
+        print(f'{dest} already exists')
+        os.remove(dest)
+
+    dest.symlink_to(src)
+    print(f'{path} -> {dest}')
+
+
+def iterdir(path):
+    for file in path.iterdir():
+        if file.name in black_list:
+            continue
+        if file.is_dir():
+            (install_dest / file).mkdir(exist_ok=True)
+            iterdir(file)
+        elif file.is_file() or file.is_symlink():
+            mk_symlink(file)
+        
+
+def main():
+    iterdir(pathlib.Path('.'))
+
+if __name__ == "__main__":
+    main()
